6fd87511a21e9c96eb649527ff157247c35ee146
xy19xiaoyu <0714010877@163.com>
2020-10-19 18:18:39 +0800

    init

---
+++ JD_seckill/FrmMain.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace JD_seckill
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tstxtUrl = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tsbGo = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbAuto = new System.Windows.Forms.ToolStripButton();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsbMsg = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.tstxtUrl,
+            this.tsbGo,
+            this.toolStripSeparator1,
+            this.tsbAuto,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.SizeChanged += new System.EventHandler(this.toolStrip1_SizeChanged);
+            // 
+            // tstxtUrl
+            // 
+            this.tstxtUrl.AutoSize = false;
+            this.tstxtUrl.MergeIndex = 2;
+            this.tstxtUrl.Name = "tstxtUrl";
+            this.tstxtUrl.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(44, 22);
+            this.toolStripLabel1.Text = "地址：";
+            // 
+            // tsbGo
+            // 
+            this.tsbGo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbGo.Image = ((System.Drawing.Image)(resources.GetObject("tsbGo.Image")));
+            this.tsbGo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbGo.Name = "tsbGo";
+            this.tsbGo.Size = new System.Drawing.Size(28, 22);
+            this.tsbGo.Text = "go";
+            this.tsbGo.Click += new System.EventHandler(this.tsbGo_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsbAuto
+            // 
+            this.tsbAuto.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbAuto.Image = ((System.Drawing.Image)(resources.GetObject("tsbAuto.Image")));
+            this.tsbAuto.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbAuto.Name = "tsbAuto";
+            this.tsbAuto.Size = new System.Drawing.Size(84, 22);
+            this.tsbAuto.Text = "开始自动秒杀";
+            this.tsbAuto.Click += new System.EventHandler(this.tsbAuto_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 25);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 425);
+            this.webBrowser1.TabIndex = 6;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbMsg});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsbMsg
+            // 
+            this.tsbMsg.Name = "tsbMsg";
+            this.tsbMsg.Size = new System.Drawing.Size(52, 17);
+            this.tsbMsg.Text = "tsbMsg";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(36, 22);
+            this.toolStripButton1.Text = "停止";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripTextBox tstxtUrl;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton tsbGo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsbAuto;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsbMsg;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ JD_seckill/FrmMain.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace JD_seckill
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStrip1_SizeChanged(object sender, EventArgs e)
+        {
+            tstxtUrl.Width = toolStrip1.Width - 200;
+        }
+
+        private void tsbGo_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(tstxtUrl.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tsbMsg.Text = webBrowser1.Version.ToString();
+            webBrowser1.DocumentTitleChanged += WebBrowser1_DocumentTitleChanged;
+            webBrowser1.DocumentCompleted += WebBrowser1_DocumentCompleted;
+            webBrowser1.Navigating += WebBrowser1_Navigating;
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+        private bool isCompleted = false;
+        private void WebBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            isCompleted = false;
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            isCompleted = true;
+        }
+
+        private void WebBrowser1_DocumentTitleChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsbAuto_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        public bool hasClicked = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (webBrowser1.DocumentTitle)
+            {
+                case "京东-欢迎登录":
+                    this.tsbMsg.Text = "请登录";
+                    return;
+                case "商品已成功加入购物车":
+                    var GotoShoppingCart = webBrowser1.Document.GetElementById("GotoShoppingCart");
+                    GotoShoppingCart?.InvokeMember("click");
+                    tsbMsg.Text = "点击去购物车";
+                    break;
+                case "京东商城 - 购物车":
+                    //去结算
+                    var GoPay = webBrowser1.Document.GetElementsByTagName("A");
+                    foreach (HtmlElement item_A in GoPay)
+                    {
+                        if (item_A.OuterText == null) continue;
+                        if (item_A.GetAttribute("className") == "common-submit-btn")
+                        {
+
+                            Thread.Sleep(100);
+                            if (!isCompleted) return;
+                            item_A.InvokeMember("click");
+                            tsbMsg.Text = "点击-去结算";
+                            hasClicked = true;
+                            break;
+                        }
+                    }
+                    break;
+                case "订单结算页 -京东商城":
+                    //提交订单
+                    var order_submit = webBrowser1.Document.GetElementById("order-submit");
+                    order_submit?.InvokeMember("click");
+                    tsbMsg.Text = "点击-订单提交";
+                    break;
+                default:
+                    var items = webBrowser1.Document.GetElementsByTagName("div");
+                    foreach (HtmlElement item in items)
+                    {
+                        if (item.OuterText == null) continue;
+                        if (item.GetAttribute("className") != "activity-message")
+                        {
+                            continue;
+                        }
+
+                        if (item.OuterText.Contains("结束"))
+                        {
+                            //添加到购物车
+                            var InitCartUrl = webBrowser1.Document.GetElementById("InitCartUrl");
+                            InitCartUrl?.InvokeMember("click");
+                            tsbMsg.Text = "点击-添加到购物车";
+                            break;
+                        }
+                        else
+                        {
+                            tsbMsg.Text = item.OuterText;
+
+                        }
+                        break;
+                    }
+                    return;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+        }
+    }
+}
